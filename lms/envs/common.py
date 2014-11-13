1f6ce15a11fbd1cea13b02d38da3252c81e3c324
Chris Rossi <chris@archimedeanco.com>
2014-11-13 14:58:19 -0500

    Add the 'POC Coach' course role. scrumdo #18

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -184,6 +184,9 @@ FEATURES = {
     # the setting FIELD_OVERRIDE_PROVIDERS.
     'INDIVIDUAL_DUE_DATES': False,
 
+    # Enable Personal Online Courses
+    'PERSONAL_ONLINE_COURSES': False,
+
     # Enable legacy instructor dashboard
     'ENABLE_INSTRUCTOR_LEGACY_DASHBOARD': True,
     # Is this an edX-owned domain? (used on instructor dashboard)
