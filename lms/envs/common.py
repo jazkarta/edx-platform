71e15ddf6ee1854e37f6e453cb5129ead7bab5a9
Carlos de la Guardia <cguardia@yahoo.com>
2015-01-14 12:54:48 -0600

    initial attempt at js refactoring using backbone.js

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1127,6 +1127,8 @@ verify_student_js = [
     'js/verify_student/pay_and_verify.js',
 ]
 
+pocs_js = sorted(rooted_glob(PROJECT_ROOT / 'static', 'js/pocs/**/*.js'))
+
 PIPELINE_CSS = {
     'style-vendor': {
         'source_filenames': [
@@ -1311,7 +1313,11 @@ PIPELINE_JS = {
     'verify_student': {
         'source_filenames': verify_student_js,
         'output_filename': 'js/verify_student.js'
-    }
+    },
+    'pocs': {
+        'source_filenames': pocs_js,
+        'output_filename': 'js/pocs.js'
+    },
 }
 
 PIPELINE_DISABLE_WRAPPER = True
